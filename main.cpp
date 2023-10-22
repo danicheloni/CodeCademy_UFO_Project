988a579fe59610912fe0e1a6c1bdbaa7d836d990
danicheloni <danielascheloni@gmail.com>
2023-10-22 11:35:38 -0300

    finish project

--- main.cpp
+++ main.cpp
@@ -3,25 +3,45 @@
 
 int main() {
   
-    greet();
+  greet();
 
-    /*std::string codeword = "codecademy";
-    std::string answer = "__________";
-    int misses = 0;
+  std::string codeword = "codecademy";
+  std::string answer = "__________";
+  int misses = 0;
 
-    std::vector<char> incorrect;
-    bool guess = false;
+  std::vector<char> incorrect;
+  bool guess = false;
+  char letter;
+  
+  while (answer != codeword && misses < 7) {
+    display_misses(misses);
+    display_status(incorrect, answer);
 
+    std::cout << "\n\nPlease enter your guess: " << "\n";
+    std::cin >> letter;
 
-    while (answer != codeword && misses < 7) {
-        display_misses(misses);
-        display_status(incorrect, answer);
-
-
-        misses++;
+    for (int i = 0; i < codeword.length(); i++) {
+      if (letter == codeword[i]) {
+        answer[i] = letter;
+        guess = true;
+      }
     }
-    end_game(answer, codeword);  
+    if (guess == true) {
+      std::cout << "\nCorrect! You are closer to cracking the code word\n";
+    }
+    else {
+      std::cout << "\nIncorrect! The tractor beam pulls the person in further. \n";
+      incorrect.push_back(letter);
 
-*/
-    return 0;
+      misses++;
+    } 
+    guess = false; 
+
+
+
+
+    
+  }
+  end_game(answer, codeword);  
+return 0; 
 }//end of main
