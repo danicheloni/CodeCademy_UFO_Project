a478855ebb9df629d2bf81bdcc455d7acbb27692
danicheloni <danielascheloni@gmail.com>
2023-10-21 21:14:05 -0300

    add files

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+  
+    greet();
+
+    /*std::string codeword = "codecademy";
+    std::string answer = "__________";
+    int misses = 0;
+
+    std::vector<char> incorrect;
+    bool guess = false;
+
+
+    while (answer != codeword && misses < 7) {
+        display_misses(misses);
+        display_status(incorrect, answer);
+
+
+        misses++;
+    }
+    end_game(answer, codeword);  
+
+*/
+    return 0;
+}//end of main
